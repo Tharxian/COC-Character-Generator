a6bf44d69bccae3f9fb0e623d37ef7cf55726512
Tharxian <117448098+Tharxian@users.noreply.github.com>
2023-08-10 15:27:25 +0800

    COCCG.py 1.0 version

--- COCCG.py
+++ COCCG.py
@@ -1,9 +1,16 @@
 '''Yifan Ge (Tharxian) all rights reserved'''
-print('©tharxian 2023')
-print('Source code available at:https://github.com/Tharxian/COC-Character-Generator/blob/main/COCCG.py')
-
 import random
 import math
+import cocdice
+import os
+import COCkp
+from docu import prepare_text,write_to_docx
+
+
+os.system('clear')
+
+print('COC character generator ©tharxian 2023')
+
 
 def dice6(num = [0,0,0],time = 1):
     roll = []
@@ -13,6 +20,11 @@ def dice6(num = [0,0,0],time = 1):
     for x in range(time):
         result += roll[num[x]]
     return result
+
+def kp_line(txt):
+    print("\033[31mKeeper 📖: \033[0m")
+    print(txt)
+    print("\033[33m----------------------------------\033[0m")
         
 # 角色名字
 print("\033[33m----------------------------------\033[0m")
@@ -64,11 +76,12 @@ print("\033[31mKeeper 📖: \033[0m")
 print("Your fate is destinied!")
 input("Press ENTER to continue.")
 print("\033[33m----------------------------------\033[0m")
+os.system('clear')
 
 # Output
 print('Name: ' + "\033[36m" + char_name + "\033[0m")
-age = math.ceil(random.uniform(10,90))
-print('Age: ' + "\033[36m" + str(age) + "\033[0m")
+AGE = math.ceil(random.uniform(10,90))
+print('Age: ' + "\033[36m" + str(AGE) + "\033[0m")
 print('')
 
 termset1 = ['STR','CON','DEX','APP','POW','LUK']
@@ -77,20 +90,20 @@ termdict1 = {'STR':0,'CON':0,'DEX':0,'APP':0,'POW':0,'LUK':0}
 for things in termset1:
     outcome = dice6(num,3)*5
     termdict1[things] = str(outcome)
-    print(things + ":___/" "\033[32m" + termdict1[things] + "\033[0m") 
+    print(things +': ' +  "\033[32m" + termdict1[things] + "\033[0m") 
     
 termset2 = ['SIZ','EDU','INT']
 termdict2 = {'SIZ':0,'EDU':0,'INT':0}
 for things in termset2:
     outcome = (dice6(num,2)+6)*5
     termdict2[things] = str(outcome)
-    print(things + ":___/" "\033[32m" + termdict2[things] + "\033[0m") 
+    print(things +  ': ' + "\033[32m" + termdict2[things] + "\033[0m") 
 
 HP = math.ceil((int(termdict2['SIZ'])+int(termdict1['CON']))/10)
 
 print(' ')
-print(' HP:___/' + "\033[32m" + str(HP) + "\033[0m")
-print('SAN:___/' + "\033[32m" + termdict1['POW'] + "\033[0m")
+print(' HP: ' + "\033[32m" + str(HP) + "\033[0m")
+print('SAN: ' + "\033[32m" + termdict1['POW'] + "\033[0m")
 
 # 判断movement，比较STR，DEX和SIZ
 case1 = termdict1['STR'] > termdict2['SIZ']
@@ -108,16 +121,29 @@ else:
     
 print('MOV：' + "\033[32m" + str(MOV) + "\033[0m")
 print(' ')
+
+# 写入word文档
+writing = prepare_text(char_name,termdict1,termdict2,HP,MOV,AGE)
+doc = char_name + ' Profile.docx'
+write_to_docx(writing,'character/' + doc)
+
+# 写入csv
+COCkp.char_log(char_name,termdict1,termdict2,HP,MOV)
+
+
 goon = input('Press ENTER to end  ')
 
 if goon == 'ENTER':
-    print("\033[33m----------------------------------\033[0m")
-    print("\033[35mUnknown voice ❓: \033[0m")
-    input("Initiating simulating fight...")
-    print("\033[33m----------------------------------\033[0m")
-    print("\033[35mUnknown voice ❓: \033[0m")
-    input("Simulating fight is still under develop😊, thank you for testing.")
-    print("\033[33m----------------------------------\033[0m")
+    from sim_fight import fight
+    fight()
+
+    
+    
+    
+    
+    
+    
+    
     
     
 
